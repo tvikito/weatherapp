81582f0bce697491e7b872d946fe1b9eba6997c6
Tomas Vykoukal <tomas.vykoukal@sinnerschrader.com>
2019-09-17 22:39:12 +0200

    Updates

---
+++ precache-manifest.262a3225c6277f7cad8fdc7eeddb675b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71e7231fd8be8596d76de88bf6b9a7f",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "66d2535f28a4b9c9f2c4",
+    "url": "/weatherapp/static/css/main.1813ecf1.chunk.css"
+  },
+  {
+    "revision": "63682a73511ace73d0f2",
+    "url": "/weatherapp/static/js/2.e58832d3.chunk.js"
+  },
+  {
+    "revision": "66d2535f28a4b9c9f2c4",
+    "url": "/weatherapp/static/js/main.4c860a8d.chunk.js"
+  },
+  {
+    "revision": "bf16f4863ae6c35078f6",
+    "url": "/weatherapp/static/js/runtime~main.c2cefd07.js"
+  }
+]);
